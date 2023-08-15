3e389af14c27ad7c89f491f8096b7acc4f06faa4
ashutosh720 <ashutoshrana1999@gmail.com>
2023-08-15 20:15:21 +0530

    Initial commit

---
+++ PongGame/src/Main.java
@@ -0,0 +1,25 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        GameFrame frame =new GameFrame();
+
+    }
+}
+
+
+
+// Press Alt+Enter with your caret at the highlighted text to see how
+// IntelliJ IDEA suggests fixing it.
+        /*System.out.printf("Hello and welcome!");
+
+                // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+                for (int i = 1; i <= 5; i++) {
+
+                // Press Shift+F9 to start debugging your code. We have set one breakpoint
+                // for you, but you can always add more by pressing Ctrl+F8.
+                System.out.println("i = " + i);*/
